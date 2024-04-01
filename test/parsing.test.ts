3f1a37ea8037c7119f2ea05e4d8da03d0e358262
3w36zj6 <52315048+3w36zj6@users.noreply.github.com>
2024-04-01 23:45:17 +0900

    test: Add parsing test

---
+++ test/parsing.test.ts
@@ -0,0 +1,28 @@
+// parse all fixture and should has
+import { test } from "@textlint/ast-tester";
+import fs from "node:fs";
+import path from "node:path";
+import { describe, expect, it } from "vitest";
+import { parse } from "../src/parse";
+
+describe("parsing", () => {
+	const fixtureDir = path.join(__dirname, "fixtures");
+	for (const filePath of fs.readdirSync(fixtureDir)) {
+		const dirName = path.basename(filePath);
+		it(`${dirName} match AST`, () => {
+			const input = fs.readFileSync(
+				path.join(fixtureDir, filePath, "input.mdx"),
+				"utf-8",
+			);
+			const AST = parse(input);
+			test(AST);
+			const output = JSON.parse(
+				fs.readFileSync(
+					path.join(fixtureDir, filePath, "output.json"),
+					"utf-8",
+				),
+			);
+			expect(AST).toEqual(output);
+		});
+	}
+});
